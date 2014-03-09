5a53df712bc6f9729078394669f4910536a225b3
Spferical <spferical@gmail.com>
2014-03-09 14:21:56 -0400

    Switched to a non-square font for better readability.
    Being non-square is annoying, but kerning /was/ one of the complaints
    about FtF last year.

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import ui
 from config import SCREEN_WIDTH, SCREEN_HEIGHT, MAX_FPS, GAME_NAME
 
 def main():
+    tcod.console_set_custom_font(
+            'terminal10x16_gs_ro.png',
+            flags=tcod.FONT_LAYOUT_ASCII_INROW)
     tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_NAME,
                         False)
     tcod.sys_set_fps(MAX_FPS)
